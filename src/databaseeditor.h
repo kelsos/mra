508e493af2241e97e8147016d705bc8c8d5d9d1e
Konstantinos Paparas <kelsos86@gmail.com>
2012-05-31 00:24:10 +0300

    Restores accidentaly removed code.

---
+++ src/databaseeditor.h
@@ -0,0 +1,68 @@
+#ifndef DATABASEEDITOR_H
+#define DATABASEEDITOR_H
+
+#include <QDialog>
+#include <QDebug>
+#include <QStringListModel>
+#include <QFileDialog>
+#include <QBuffer>
+#include <QGraphicsScene>
+#include "genresinfoeditor.h"
+#include "Data/databasemanager.h"
+#include "MangaInfo.h"
+
+namespace Ui
+{
+class DatabaseEditor;
+}
+
+class DatabaseEditor : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DatabaseEditor(QWidget *parent = 0);
+    ~DatabaseEditor();
+
+private:
+    Ui::DatabaseEditor *ui;
+    void retrieveCover(QString selectionTitle);
+    void updateStatusCombobox(QString status);
+    void updateMangaAuthorData(QString selection);
+    void updateMangaGenreData(QString selection);
+    void updatePublisherData(QString selection);
+    QGraphicsScene* scene;
+    QStringListModel *qModel;
+    QStringListModel *genresModel;
+    bool isNewMangaEntryInProgress;
+	bool isNewAuthorEntryInProgress;
+	bool isNewPublisherEntryInProgress;
+    QByteArray coverArray;
+
+private slots:
+    void openGenresEdit();
+    void handleMangaComboIndexChanged(QString text);
+    void handleAuthorComboIndexChanged(QString text);
+    void handlePublisherComboIndexChanged(QString text);
+    void listAllManga();
+	void listAllAuthors();
+	void listAllPublishers();
+    void addSelectAuthorToManga();
+    void addSelectPublisherToManga();
+    void addSelecteGenreToManga();
+    void removeSelectedAuthorFromManga();
+    void removeSelectedGenreFromManga();
+    void removeSelectedPublisherFromManga();
+    void openImageSelectionDialog();
+    void newManga();
+    void saveManga();
+    void deleteManga();
+    void newAuthor();
+    void saveAuthor();
+    void deleteAuthor();
+    void newPublisher();
+    void savePublisher();
+    void deletePublisher();
+};
+
+#endif // DATABASEEDITOR_H
--- src/form_user_login.h
+++ src/form_user_login.h
@@ -1,25 +1,3 @@
-Copyright (c) 2012, Konstantinos Paparas
-All rights reserved.
-
-Redistribution and use in binary form, without modification, is permitted provided that 
-the following conditions are met:
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-      
-      Redistribution and use in source form is prohibited.	  
-      
-      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-      ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-      WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-      DISCLAIMED. IN NO EVENT SHALL Konstantinos Paparas BE LIABLE FOR ANY
-      DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-      (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 #ifndef FORM_USER_LOGIN_H
 #define FORM_USER_LOGIN_H
 
@@ -43,6 +21,9 @@ public:
     
 private:
     Ui::form_user_login *ui;
+
+public slots:
+
 };
 
 #endif // FORM_USER_LOGIN_H
